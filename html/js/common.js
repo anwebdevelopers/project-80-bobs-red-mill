c615cb85eb9123ec007b0844c62b30c51f413cc6
anwebdevelopers <anwebdev78@gmail.com>
2018-12-15 12:53:25 +0400

    15.12.2018

--- html/js/common.js
+++ html/js/common.js
@@ -51,7 +51,7 @@ $(function() {
     $('.tabs').each(function() {
         const $this = $(this);
 
-        if ( ! $this.find('.tabs__buttons').length ) {
+        if (!$this.find('.tabs__buttons').length) {
             $this.prepend('<div class="tabs__buttons"></div>');
         }
 
@@ -84,11 +84,11 @@ $(function() {
             }
 
             $this.append(
-                '<div class="recipes-home__next">'+
-                    '<figure class="recipes-home__next-img">'+
-                        '<img src="' + url + '">'+
-                    '</figure>'+
-                    '<div class="recipes-home__next-text">Следующий рецепт</div>'+
+                '<div class="recipes-home__next">' +
+                    '<figure class="recipes-home__next-img">' +
+                        '<img src="' + url + '">' +
+                    '</figure>' +
+                '<div class="recipes-home__next-text">Следующий рецепт</div>' +
                 '</div>'
             );
 
@@ -114,11 +114,10 @@ $(function() {
     /*******************************************************/
     $('.page__aside-nav-item').addClass('accordion').find('.page__aside-nav-title').addClass('accordion__button').siblings('.page__aside-nav-list').addClass('accordion__box');
     const $accordion = $('.accordion');
-    $accordion.on('click', '.accordion__button', function(e)
-    {
+    $accordion.on('click', '.accordion__button', function(e) {
         e.stopPropagation();
 
-        if ($window.width() <= 640) {
+        if ($window.width() <= 768) {
 
             const $this = $(this);
 
@@ -189,60 +188,189 @@ $(function() {
     /*******************************************************/
     //YANDEX MAP
     /*******************************************************/
-    $('#map').length && ymaps.ready(function() {
 
-        const myMap = new ymaps.Map('map', {
-            center: [59.521582, 34.435892],
-            zoom: 14,
-            controls: [],
-            behaviors: ['drag', 'dblClickZoom', 'rightMouseButtonMagnifier', 'multiTouch']
-        }, {
-            searchControlProvider: 'yandex#search'
-        });
+    if ((typeof ymaps === 'object') && $('#map').length) {
+        // Пример реализации боковой панели на основе наследования от collection.Item.
+        // Боковая панель отображает информацию, которую мы ей передали.
+        ymaps.modules.define('Panel', [
+            'util.augment',
+            'collection.Item'
+        ], function(provide, augment, item) {
+            // Создаем собственный класс.
+            var Panel = function(options) {
+                Panel.superclass.constructor.call(this, options);
+            };
 
-        //Элементы управления
-        myMap.controls.add('zoomControl', {
-            size: 'small',
-            position: {
-                top: 'auto',
-                left: 10,
-                bottom: 50
-            }
-        });
+            // И наследуем его от collection.Item.
+            augment(Panel, item, {
+                onAddToMap: function(map) {
+                    Panel.superclass.onAddToMap.call(this, map);
+                    this.getParent().getChildElement(this).then(this._onGetChildElement, this);
+                    // Добавим отступы на карту.
+                    // Отступы могут учитываться при установке текущей видимой области карты,
+                    // чтобы добиться наилучшего отображения данных на карте.
+                    map.margin.addArea({
+                        // top: 'auto',
+                        right: 30,
+                        bottom: 40,
+                        width: '270px',
+                        height: '400px'
+                    })
+                },
 
-        //По клику ставим метку и перемещаемся к ней
-        $('[href="#map"]').on('click', function() {
+                onRemoveFromMap: function(oldMap) {
+                    if (this._$control) {
+                        this._$control.remove();
+                    }
+                    Panel.superclass.onRemoveFromMap.call(this, oldMap);
+                },
 
-            const latitude = Number($(this).attr('data-map-latitude'));
-            const longitude = Number($(this).attr('data-map-longitude'));
-            const text = $(this).attr('data-map-text');
-
-            //Отображение метки с контентом
-            myMap.geoObjects.add(new ymaps.Placemark([latitude, longitude], {
-                // iconLayout: 'default#image',
-                // iconImageHref: 'img/icon-map3.png',
-                // iconImageSize: [34, 48],
-                // iconImageOffset: [-17, -48]
-                // iconContent: text,
-                // hintContent: text,
-                balloonContent: text
-            }, {
-                preset: "islands#blueStretchyIcon"
-            }));
-
-            //перемещение по координатам
-            myMap.panTo([latitude, longitude], {
-                flying: false,
-                duration: 1300
+                _onGetChildElement: function(parentDomContainer) {
+                    // Создаем HTML-элемент с текстом.
+                    // По-умолчанию HTML-элемент скрыт.
+                    this._$control = $('<div class="map__info"><div class="container"><div class="map__info-inner"><div class="map__info-close"></div><div class="map__info-content"></div></div></div></div>').appendTo(parentDomContainer);
+                    this._$content = $('.map__info-content');
+                    // При клике по крестику будем скрывать панель.
+                    $('.map__info-close').on('click', this._onClose);
+                },
+                _onClose: function() {
+                    $('.map__info').css('display', 'none');
+                    $('.map__info-content').html('');
+                },
+                // Метод задания контента панели.
+                setContent: function(text) {
+                    // При задании контента будем показывать панель.
+                    this._$control.css('display', 'block');
+                    this._$content.html(text);
+                }
             });
-            // myMap.setZoom(16)
+
+            provide(Panel);
         });
 
-        //Вкл/Выкл драг карты при адаптиве
-        $(window).on('resize load', function() {
-            $(window).width() <= 960 ? myMap.behaviors.disable('drag') : myMap.behaviors.enable('drag')
-        });
+        ymaps.ready(['Panel']).then(function() {
 
-    });
+            const map = new ymaps.Map("map", {
+                center: [43.251258, 76.931046],
+                zoom: 13,
+                controls: [],
+                behaviors: ['drag', 'dblClickZoom', 'rightMouseButtonMagnifier', 'multiTouch']
+            }, {
+                searchControlProvider: 'yandex#search'
+            });
+
+            //Элементы управления
+            map.controls.add('zoomControl', {
+                size: 'small',
+                position: {
+                    top: 'auto',
+                    left: 10,
+                    bottom: 40
+                }
+            });
+
+            // Создадим контент для меток.
+            const panelContentArr = [
+                {
+                    сoordinates: [43.251258, 76.931046],
+                    image: 'img/img-map-info1.png',
+                    title: 'ТЦ “Алтын Тулпар”',
+                    address: 'ул. Лейтенанта Федотова д.32 оф. 300, третий этаж',
+                    tel: '+7 777 454 55 44',
+                    mode: ['<b>Пн-Пт</b> 10:00 – 19:00', '<b>Сб-Вс</b> 12:00 – 22:00'],
+                },
+                {
+                    сoordinates: [43.261258, 76.941046],
+                    image: 'img/img-map-info2.png',
+                    title: 'ТЦ “Алтын Тулпар”',
+                    address: 'Улица Муканова д.32 оф. 300, третий этаж',
+                    tel: '+7 777 454 55 44',
+                    mode: ['<b>Пн-Пт</b> 10:00 – 19:00', '<b>Сб-Вс</b> 12:00 – 22:00'],
+                },
+            ];
+            let startPanelContent;
+
+            const panel = new ymaps.Panel();
+
+            map.controls.add(panel, {
+                float: 'none',
+                position: {
+                    left: 0,
+                    right: 0,
+                    bottom: 40,
+                }
+            });
+
+            // Создадим коллекцию геообъектов.
+            const collection = new ymaps.GeoObjectCollection(null, {
+                // Запретим появление балуна.
+                hasBalloon: false,
+                iconColor: '#3b5998'
+            });
+
+            for (let i = 0; i < panelContentArr.length; i++) {
+                let panelHTML = '';
+
+                panelHTML += panelContentArr[i].image ? '<figure class="map__info-img"><img src="' + panelContentArr[i].image + '" alt="' + (panelContentArr[i].title ? panelContentArr[i].title : '') + '"></figure>' : '';
+
+                panelHTML += panelContentArr[i].title ? '<h3 class="map__info-title">' + panelContentArr[i].title + '</h3>' : '';
+
+                panelHTML += panelContentArr[i].address ? '<address class="map__info-address">' + panelContentArr[i].address + '</address>' : '';
+
+                panelHTML += panelContentArr[i].tel ? '<a href="tel:' + panelContentArr[i].tel.replace(/\s+/g, '') + '" class="map__info-tel">' + panelContentArr[i].tel + '</a>' : '';
+
+                if (panelContentArr[i].mode) {
+                    panelHTML += '<div class="map__info-mode">';
+
+                    for (let y = 0; y < panelContentArr[i].mode.length; y++) {
+                        panelHTML += panelContentArr[i].mode[y] ?  panelContentArr[i].mode[y] + '<br>' : '';
+                    }
+
+                    panelHTML += '</div>';
+                }
+
+                if (i === 0) {
+                    startPanelContent = panelHTML;
+                }
+
+                // Добавим геообъекты в коллекцию.
+                collection.add(new ymaps.Placemark(panelContentArr[i].сoordinates, {
+                    balloonContent: panelHTML,
+                    hintContent: panelContentArr[i].title,
+                }, {
+                    iconLayout: 'default#image',
+                    iconImageHref: 'img/icon-mark.svg',
+                    iconImageSize: [37, 56],
+                    iconImageOffset: [-18, -56],
+                }));
+
+            }
+
+            // Добавим коллекцию на карту.
+            map.geoObjects.add(collection);
+            // Подпишемся на событие клика по коллекции.
+            collection.events.add('click', function(e) {
+                // Получим ссылку на геообъект, по которому кликнул пользователь.
+                const target = e.get('target');
+                // Зададим контент боковой панели.
+                panel.setContent(target.properties.get('balloonContent'));
+                // Переместим центр карты по координатам метки с учётом заданных отступов.
+                map.panTo(target.geometry.getCoordinates(), {
+                    useMapMargin: true
+                });
+            });
+
+            //Вкл/Выкл драг карты при адаптиве
+            $window.on('resize load', function() {
+                $window.width() <= 992 ? map.behaviors.disable('drag') : map.behaviors.enable('drag')
+            });
+
+            //Установим стартовый контент в панель
+            setTimeout(function() {
+                panel.setContent(startPanelContent);
+            }, 1000);
+
+        });
+    }
 
 });
